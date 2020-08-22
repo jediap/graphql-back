87c5db2b1c22391363a4a5407eac7dded5fc08d3
jdiaz <jdiaz@belikesoftware.com>
2020-08-22 20:04:02 +0200

    refactor code

--- src/main/java/com/example/graphql/controller/UserController.java
+++ src/main/java/com/example/graphql/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.example.graphql.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping(value = "user")
-    public String getCities() {
-        return "hola test";
+    @PostMapping(value = "user")
+    public String createUsers() {
+        return "new user";
     }
 
 }
